a1c1c376c6c23ea54cd6373c7af099d838660ced
adroxk <adroxk@gmail.com>
2026-02-16 12:53:48 -0600

    Primer Deploy Amigurumis

---
+++ src/data/productos.js
@@ -0,0 +1,20 @@
+
+import Osito from '../assets/amigurumiOsito.png';
+import Conejito from '../assets/amigurummiConejito.png';
+
+export const productos = [
+    {
+        id: 1,
+        nombre: "Osito",
+        descripcion: "Amigurumi tejido a mano.",
+        imagen: Osito,
+        precio: 500
+    },
+    {
+        id: 2,
+        nombre: "Conejito",
+        descripcion: "Suave y adorable.",
+        imagen: Conejito,
+        precio: 600
+    }
+]
