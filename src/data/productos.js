bd5c0bac69f43b0ce7458b081a232b80c69f5b69
adroxk <adroxk@gmail.com>
2026-02-17 21:31:02 -0600

    Actualizacion 3

--- src/data/productos.js
+++ src/data/productos.js
@@ -1,6 +1,8 @@
 
 import Osito from '../assets/amigurumiOsito.png';
 import Conejito from '../assets/amigurummiConejito.png';
+import Dinosaurio from '../assets/amigurumiDinosaurio.png';
+
 
 export const productos = [
     {
@@ -16,5 +18,12 @@ export const productos = [
         descripcion: "Suave y adorable.",
         imagen: Conejito,
         precio: 600
+    },
+    {
+        id: 3,
+        nombre: "Dinosaurio",
+        descripcion: "Un amigurumi divertido y colorido.",
+        imagen: Dinosaurio,
+        precio: 550
     }
 ]
